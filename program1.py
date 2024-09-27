1b8f58985f6c1ceae7ddb1a8761577d2935376d6
Hemakumari9931 <hemakumarimnj@gmail.com>
2024-09-27 15:46:43 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,10 +1,31 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
+def is_valid_parentheses(s: str) -> bool:
+    
+    stack = []
+    
+    bracket_map = {')': '(', '}': '{', ']': '['}
+
+    for char in s:
+       
+        if char in bracket_map:
+           
+            top_element = stack.pop() if stack else '#'
+           
+            if bracket_map[char] != top_element:
+                return False
+        else:
+            # If it's an opening bracket, push it onto the stack
+            stack.append(char)
+
+
+    return not stack
+
+
+if __name__ == "__main__":
+    user_input = input("Enter a string of parentheses: ")
+    if is_valid_parentheses(user_input):
+        print("The input string is valid.")
+    else:
+        print("The input string is invalid.")
 
 
 
